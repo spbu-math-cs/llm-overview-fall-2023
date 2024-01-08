a9e81adbbc17749a771c1d7b2240b2f9c52d7d8b
geaned <angenispb@mail.ru>
2024-01-08 05:03:25 +0300

    Second revision of inflight batching

--- practical examples/inflight-batching/inference_bench_baseline.py
+++ practical examples/inflight-batching/inference_bench_baseline.py
@@ -1,16 +1,19 @@
-from icecream import ic
 from time import time
-from statistics import mean, stdev
 import torch
 
 from transformers import LlamaForCausalLM, LlamaTokenizerFast
 from transformers.generation import GenerationConfig
 
+from modify_generator_mixin import modify_mixin
 
-BATCH_SIZE = 3
-TEST_SIZE = 15
-MAX_NEW_TOKENS = 32
+
+BATCH_SIZE = 16
+TEST_SIZE = 64
+MAX_LENGTH = 50
 EOS_TOKEN_ID = 29889    # replaced eos token because found weights did not generate original eos token often enough
+PAD_TOKEN_ID = 0
+
+# modify_mixin()
 
 # sample querying
 model = LlamaForCausalLM.from_pretrained(
@@ -18,63 +21,43 @@ model = LlamaForCausalLM.from_pretrained(
     do_sample=True
 )
 
-# greedy querying
-# model = LlamaForCausalLM.from_pretrained(
-#     "NousResearch/Llama-2-7b-hf",
-#     do_sample=False,
-#     temperature=1,
-#     top_p=1
-# )
-
-# same inputs
 inps = ['Fun fact of the day: ']*TEST_SIZE
-
-# different inputs
-# inps = [
-#     'Fun fact of the day: ',
-#     'The capital city of Germany is ',
-#     'Eminem\'s album called ',
-#     'Tomorrow the weather is ',
-#     'Breaking news: ',
-#     'Today we will discuss '
-# ]
 
 tokenizer = LlamaTokenizerFast.from_pretrained('NousResearch/Llama-2-7b-hf')
 tokenizer.pad_token = '[PAD]'
 tokenizer.padding_side = 'left'
 
 gen_cfg = GenerationConfig.from_model_config(model.generation_config)
-gen_cfg.max_new_tokens = MAX_NEW_TOKENS
+gen_cfg.max_length = MAX_LENGTH
 gen_cfg.eos_token_id = EOS_TOKEN_ID
-gen_cfg.pad_token_id = 0
-ic(gen_cfg)
+gen_cfg.pad_token_id = PAD_TOKEN_ID
 
 res_str = list()
-st_time, end_time, tok_cnt = list(), list(), list()
+tokens_generated = 0
 
+st_time = time()
+
+# primitive batch-by-batch parsing
 for idx in range(0, TEST_SIZE, BATCH_SIZE):
-    st_time_val = time()
-    st_time.extend([st_time_val]*BATCH_SIZE)
-
     inp_tok = torch.Tensor(tokenizer(inps[idx:idx+BATCH_SIZE], padding=True)['input_ids']).to(torch.int64)
-    inp_tok_start_size = inp_tok.shape[-1]
+    start_size = inp_tok.shape[-1]
 
     res_tok = model.generate(
         inputs=inp_tok,
         generation_config=gen_cfg
     )
-    res_str.extend(tokenizer.batch_decode(res_tok, skip_special_tokens=True, clean_up_tokenization_spaces=False))
+    tokens_generated += torch.count_nonzero(res_tok[:, start_size:] != PAD_TOKEN_ID)
 
-    end_time_val = time()
-    end_time.extend([end_time_val]*BATCH_SIZE)
-    tok_cnt.extend((res_tok.shape[-1] - torch.count_nonzero(res_tok[:, inp_tok_start_size:] == 0, dim=1)).tolist())
+    res_str = tokenizer.batch_decode(res_tok, skip_special_tokens=True, clean_up_tokenization_spaces=False)
 
-diff_time = [x - y for (x, y) in zip(end_time, st_time)]
+    for idx_sub, s in enumerate(res_str):
+        print(f'{idx + idx_sub:<3}: {s}')
+
+end_time = time()
+diff_time = end_time - st_time
 
 print()
-for s in res_str:
-    print('...', s)
-print(f'Latency:          {mean(diff_time):.4f} ± {stdev(diff_time):.4f} seconds')
-print(f'Tokens generated: {sum(tok_cnt)} tokens')
-print(f'Throughput:       {sum(diff_time) / sum(tok_cnt):.4f} tokens per second')
+print(f'>>> Time taken:       {diff_time:.4f} seconds')
+print(f'>>> Tokens generated: {tokens_generated} tokens')
+print(f'>>> Throughput:       {tokens_generated / diff_time:.4f} tokens per second')
 print()
